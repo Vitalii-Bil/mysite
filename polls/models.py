67aa0a52b3e26e030c055f8a7ece5935a2fc98b3
Vitalii-Bil <bilienko223@gmail.com>
2020-12-23 23:26:35 +0200

    Flake8 and TravisCI adding

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
-from django.db import models
 import datetime
+
+from django.db import models
 from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
